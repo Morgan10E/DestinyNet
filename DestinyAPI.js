3735b415a677f2d68e3c799ff78eaa6bbe8e5ac4
Morgan Tenney <morgantenney@Morgans-MacBook-Pro.local>
2016-11-13 21:29:10 -0800

    First commit

---
+++ DestinyAPI.js
@@ -0,0 +1,65 @@
+var DestinyAPI = function(apiKey, platform) {
+  this.apiKey = apiKey;
+};
+
+DestinyAPI.prototype.DestinyHTTPRequest = function(url, callback) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    // console.log(this);
+    if (this.readyState == 4 && this.status == 200) {
+      var results = JSON.parse(this.responseText);
+      if (results.ErrorCode == 1) {
+        console.log("SUCCESSFUL REQUEST");
+        callback(results);
+      } else {
+        console.log("THERE WAS AN ERROR WITH REQUEST: " + url);
+        console.log(results.ErrorCode);
+        console.log(results.ErrorStatus);
+      }
+    } else {
+      console.log("REQUEST NOT COMPLETED: " + url);
+    }
+  }
+
+  xhttp.open("GET", "https://www.bungie.net/Platform/Destiny/" + url, true);
+  xhttp.setRequestHeader("X-Api-Key", this.apiKey);
+  xhttp.send();
+};
+
+DestinyAPI.prototype.getMembershipID = function(username, callback) {
+  this.DestinyHTTPRequest("SearchDestinyPlayer/2/" + username + "/", function(response) {
+    callback(response.Response[0]);
+  });
+};
+
+DestinyAPI.prototype.getAccount = function(membershipID, callback) {
+  this.DestinyHTTPRequest("2/Account/" + membershipID + "/", function(response) {
+    callback(response.Response.data);
+  });
+};
+
+DestinyAPI.prototype.getActivity = function(membershipID, callback) {
+  var self = this;
+  this.getAccount(membershipID, function(response) {
+    var characters = response.characters;
+    var activities = [];
+
+    async.each(characters, function(character, async_callback) {
+      self.DestinyHTTPRequest("Stats/ActivityHistory/2/" + membershipID + "/" + character.characterBase.characterId + "/?mode=None", function(response_c) {
+        var charActivities = response_c.Response.data.activities;
+        // console.log(charActivities);
+        activities = activities.concat(charActivities);
+        // console.log(activities);
+        async_callback();
+      });
+    }, function(err) {
+      if (err) {
+        console.log("Error getting activities for " + membershipID);
+      } else {
+        // console.log("DONE");
+        callback(activities);
+      }
+    });
+  });
+  // this.DestinyHTTPRequest("/Destiny/Stats/ActivityHistory/2/" + membershipID + "/"{characterId}/
+};
