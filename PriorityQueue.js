2320e4b876d716303eaaf1a7738353973277fb70
Morgan Tenney <morgantenney@DN525ihf.SUNet>
2016-11-15 12:08:26 -0800

    Priority Queue

---
+++ PriorityQueue.js
@@ -0,0 +1,36 @@
+var PriorityQueue = function(priorityFunc) {
+  this.priority = priorityFunc; //priority is a function that takes two parameters x1 and x2. If priority(x1, x2) == true, x1 comes before x2 in the priority queue
+  this.queue = [];
+};
+
+PriorityQueue.prototype.enqueue = function(item) {
+  for (var i = 0; i < this.queue.length; i++) {
+    if (this.priority(item, this.queue[i])) {
+      this.queue.splice(i, 0, item);
+      return;
+    }
+  }
+  this.queue.push(item);
+};
+
+PriorityQueue.prototype.dequeue = function() {
+  return this.queue.shift();
+};
+
+PriorityQueue.prototype.length = function() {
+  return this.queue.length;
+};
+
+PriorityQueue.prototype.peek = function() {
+  return this.queue[0];
+};
+
+PriorityQueue.prototype.filter = function(filterFunc) { //if filterFunc(item) == true, keep the item
+  var newQueue = [];
+  for (var i = 0; i < this.queue.length; i++) {
+    if (filterFunc(this.queue[i])) {
+      newQueue.push(this.queue[i]);
+    }
+  }
+  this.queue = newQueue;
+};
